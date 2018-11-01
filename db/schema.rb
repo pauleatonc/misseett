73be0ef5e5463aafa856243679ad940a565c1e14
pauleatonc <pauleatonc@gmail.com>
2018-11-01 16:28:08 -0300

    ADD: Authentication with google

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_181403) do
+ActiveRecord::Schema.define(version: 2018_11_01_191012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_28_181403) do
     t.string "firstname"
     t.string "lastname"
     t.string "gender"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
