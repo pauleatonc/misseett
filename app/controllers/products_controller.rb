7bc51183028b13e0333adf1244ef10ba84594bba
pauleatonc <pauleatonc@gmail.com>
2018-11-26 01:40:00 -0300

    FIX/WIP: Product images dont get attached, and i run off ideas ...

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :eett, :project_traffic_id, :project_type_id, :product_type_id, :company_id, :images)
+      params.require(:product).permit(:title, :description, :eett, :project_traffic_id, :project_type_id,
+        :product_type_id, :company_id, :images)
     end
 end
