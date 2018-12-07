23e53c688121a2e287080f0fa7b177c0f6398d41
pauleatonc <pauleatonc@gmail.com>
2018-12-06 21:32:26 -0300

    FIX: steel trying to restore the google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   rolify
-  belongs_to :city
-  belongs_to :gender
   has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -14,15 +12,15 @@ class User < ApplicationRecord
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-        user = User.create(
-           email: data['email'],
-           password: Devise.friendly_token[0,20],
-           name: data['name'],
-           firstname: data['first_name'],
-           lastname:  data['last_name'],
-           avatar:  data['image']
-        )
-    end
+      unless user
+          user = User.create(name: data['name'],
+            email: data['email'],
+            firstname: data['first_name'],
+            lastname: data['last_name'],
+            avatar:  data['image'],
+            password: Devise.friendly_token[0,20]
+          )
+      end
+    user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "landing#index"
+  root to: "landing#index"
 
 end
