7fb01d1d2241d55cce11de56883f0d03b2c18875
pauleatonc <pauleatonc@gmail.com>
2018-12-31 15:14:28 -0300

    ADD: DataTables by yarn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-backstrech
+//= require datatables.net/js/jquery.dataTables
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
